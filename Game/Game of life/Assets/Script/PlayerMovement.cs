cf5f58a96c487b58be5afff990aedbb502f8cc15
Luecent <thomaspham012@yahoo.com.au>
2022-08-31 09:56:42 +1000

    coding

---
+++ Game/Game of life/Assets/Script/PlayerMovement.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float moveSpeed = 5;
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.right * -moveSpeed * Time.deltaTime;
+
+        }
+
+        else if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.up * moveSpeed * Time.deltaTime;
+
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.up * -moveSpeed * Time.deltaTime;
+
+        }
+    }
+}

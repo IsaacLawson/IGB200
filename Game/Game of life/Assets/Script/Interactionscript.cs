9f2b11a591f8f09cd0c4b9af1319a90cef5ba5aa
Luecent <thomaspham012@yahoo.com.au>
2022-09-04 19:39:37 +1000

    ewfwgfw
    gwgwrgwgeg3
    4334t

--- Game/Game of life/Assets/Script/Interactionscript.cs
+++ Game/Game of life/Assets/Script/Interactionscript.cs
@@ -7,12 +7,15 @@ public class Interactionscript : MonoBehaviour
 {
     public TMP_Text interacttxt;
     public GameObject other;
+    public bool interat;
 
     // Start is called before the first frame update
     void Start()
     {
         interacttxt.enabled = false;
+        interat = false;
     }
+<<<<<<< Updated upstream
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Interactable")
@@ -33,6 +36,48 @@ public class Interactionscript : MonoBehaviour
 
         
 
+=======
+
+    void update()
+    {
+        {
+            if (interat == true && Input.GetButtonDown("Fire1"))
+            {
+                Application.LoadLevel(0);
+
+            }
+
+        }
+        //whatever();
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Interactable")
+        {
+            Debug.Log(other.gameObject.tag);
+            interacttxt.enabled = true;
+            interat = true;
+        }
+
+
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+
+
+        interacttxt.enabled = false;
+        interat = false;
+    }
+
+    void whatever()
+    {
+        if (interat == true && Input.GetButtonDown("Fire1"))
+        {
+            Application.LoadLevel(0);
+
+        }
+>>>>>>> Stashed changes
 
     }
 }

5b23d2b48630d363455a749f2a81723b981c5315
Luecent <thomaspham012@yahoo.com.au>
2022-09-21 04:00:28 +1000

    minor update to the script for quiz mini game
    wvefwfwf

---
+++ Game/Game of life/Assets/Script/Quizminigame.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quizminigame : MonoBehaviour
+{
+    public string questions;
+    public string[] answer;
+    public int correctanswer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
